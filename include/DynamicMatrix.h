5339792a73e5ff7b4ff0efa19e3b8636d401140b
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2016-04-01 23:03:14 +0200

    Update DynamicMatrix.h

--- include/DynamicMatrix.h
+++ include/DynamicMatrix.h
@@ -24,7 +24,7 @@ public:
   //! @param mat      Matrix as initializer_list
   //! @returns Current instance
   //! @throws Anything std::copy can throw
-  DynamicMatrix& operator=(std::initializer_list<T> mat); // TODO - Fix const bug
+  DynamicMatrix& operator=(std::initializer_list<T> const& mat); // TODO - Fix const bug
 
   //! @brief Getter for matrix width
   //! @returns Matrix width
@@ -75,7 +75,7 @@ DynamicMatrix<T>::DynamicMatrix(unsigned height, unsigned width, unsigned value)
   std::fill(buffer_.begin(), buffer_.end(), value);
 }
 template <typename T>
-DynamicMatrix<T>& DynamicMatrix<T>::operator=(std::initializer_list<T> mat) {
+DynamicMatrix<T>& DynamicMatrix<T>::operator=(std::initializer_list<T> const& mat) {
   assert(mat.size() == height_ * width_ && "Invalid matrix size");
   std::copy(mat.begin(), mat.end(), buffer_.begin());
   return *this;
