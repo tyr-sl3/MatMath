3fb9fde40244217d8356d79f6375f14faa451141
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2016-04-01 22:27:44 +0200

    Update DynamicMatrix.h

--- include/DynamicMatrix.h
+++ include/DynamicMatrix.h
@@ -24,7 +24,7 @@ public:
   //! @param mat      Matrix as initializer_list
   //! @returns Current instance
   //! @throws Anything std::copy can throw
-  DynamicMatrix& operator=(std::initializer_list<T> const& mat);
+  DynamicMatrix& operator=(std::initializer_list<T> mat); // TODO - Fix const bug
 
   //! @brief Getter for matrix width
   //! @returns Matrix width
